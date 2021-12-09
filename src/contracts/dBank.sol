1e7d35529172f317a5f4d25ac02d6515befd189b
ChenYunShu <92586970+ChenYunShu-01@users.noreply.github.com>
2021-12-09 21:03:48 +0800

    Add files via upload

---
+++ src/contracts/dBank.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "./Token.sol";
+
+contract dBank {
+
+  Token private token;
+
+  mapping(address => uint) public depositStart;
+  mapping(address => uint) public etherBalanceOf;
+  mapping(address => uint) public collateralEther;
+  mapping(address => uint) public feeEther;
+
+  mapping(address => bool) public isDeposited;
+  mapping(address => bool) public isBorrowed;
+
+  event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Withdraw(address indexed user, uint etherAmount, uint depositTime, uint interest);
+  event Borrow(address indexed user, uint collateralEtherAmount, uint borrowedTokenAmount);
+  event PayOff(address indexed user, uint fee);
+
+  constructor(Token _token) public {
+    token = _token;
+  }
+
+  function deposit() payable public {
+    require(isDeposited[msg.sender] == false, 'Error, deposit already active');
+    require(msg.value>=1e16, 'Error, deposit must be >= 0.01 ETH');
+
+    etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
+    depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+
+    isDeposited[msg.sender] = true; //activate deposit status
+    emit Deposit(msg.sender, msg.value, block.timestamp);
+  }
+
+  function withdraw() public {
+    require(isDeposited[msg.sender]==true, 'Error, no previous deposit');
+    uint userBalance = etherBalanceOf[msg.sender]; //for event
+
+    //check user's hold time
+    uint depositTime = block.timestamp - depositStart[msg.sender];
+    
+    // calc interest by APY(10%) and deposit duration
+    uint totalSecondsOneYear = 60 * 60 * 24 * 365;
+    uint interest = (etherBalanceOf[msg.sender] * depositTime) / (totalSecondsOneYear * 10);
+
+    //send funds to user
+    msg.sender.transfer(etherBalanceOf[msg.sender]); //eth back to user
+    token.mint(msg.sender, interest); //interest to user
+
+    //reset depositer data
+    depositStart[msg.sender] = 0;
+    etherBalanceOf[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
+
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
+  }
+
+  function borrow() payable public {
+    require(msg.value>=1e16, 'Error, collateral must be >= 0.01 ETH');
+    require(isBorrowed[msg.sender] == false, 'Error, loan already taken');
+
+    //this Ether will be locked till user payOff the loan
+    collateralEther[msg.sender] = collateralEther[msg.sender] + msg.value;
+
+    //calc tokens amount to mint, 50% of msg.value
+    uint tokensToMint = collateralEther[msg.sender] / 2;
+
+    //mint&send tokens to user
+    token.mint(msg.sender, tokensToMint);
+
+    //activate borrower's loan status
+    isBorrowed[msg.sender] = true;
+
+    emit Borrow(msg.sender, collateralEther[msg.sender], tokensToMint);
+  }
+
+  function payOff() public {
+    require(isBorrowed[msg.sender] == true, 'Error, loan not active');
+    require(token.transferFrom(msg.sender, address(this), collateralEther[msg.sender]/2), "Error, can't receive tokens"); //must approve dBank first
+
+    uint fee = collateralEther[msg.sender]/10; //calc 10% fee
+    feeEther[msg.sender] = fee;
+
+    //send user's collateral minus fee
+    msg.sender.transfer(collateralEther[msg.sender]-fee);
+
+    //reset borrower's data
+    collateralEther[msg.sender] = 0;
+    isBorrowed[msg.sender] = false;
+
+    emit PayOff(msg.sender, fee);
+  }
+}
